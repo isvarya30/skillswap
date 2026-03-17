f2e0d1fa3c6503e9dec3f74dcba22d0bd4c5f3b0
isvarya30 <isvaryaanu@gmail.com>
2026-03-17 12:53:26 +0530

    Added new files

--- humworls intern/backend/models/Skill.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const skillSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  level: { type: String, required: true },
-  description: { type: String }
-});
-
-module.exports = mongoose.model("Skill", skillSchema);
